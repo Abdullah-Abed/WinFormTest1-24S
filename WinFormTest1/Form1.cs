b52469cd131ac5bcfb9a56d6f722379df5863cb4
Abdullah-Abed <masterk7l11@gmail.com>
2024-05-08 09:56:19 +0300

    abdullah abed abed alsufyani 44004830 703

--- WinFormTest1/Form1.cs
+++ WinFormTest1/Form1.cs
@@ -11,8 +11,42 @@ namespace WinFormTest1
             textBoxes[1] = textBox2;
             textBoxes[2] = textBox3;
             textBoxes[3] = textBox4;
-        }
+
+            button1 = ButtonClockwise_Click
+            button2 = ButtonCounterClockwise_Click
+        }  
 
 
+        private void ButtonClockwise_Click(object sender, EventArgs e)
+    {
+        RotateNumbersClockwise();
     }
+
+    private void ButtonCounterClockwise_Click(object sender, EventArgs e)
+    {
+        RotateNumbersCounterClockwise();
+    }
+
+    private void RotateNumbersClockwise()
+    {
+        string temp = textBoxes[3].Text;
+        for (int i = 3; i > 0; i--)
+        {
+            textBoxes[i].Text = textBoxes[i - 1].Text;
+        }
+        textBoxes[0].Text = temp;
+    }
+
+    private void RotateNumbersCounterClockwise()
+    {
+        string temp = textBoxes[0].Text;
+        for (int i = 0; i < 3; i++)
+        {
+            textBoxes[i].Text = textBoxes[i + 1].Text;
+        }
+        textBoxes[3].Text = temp;
+    }
+
+
+}
 }
